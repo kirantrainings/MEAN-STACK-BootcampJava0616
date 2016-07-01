0b1d1e9aa3befcbbe741db72a4916667c98d1759
srinivas0190 <srinivas.kf563@gmail.com>
2016-07-01 00:51:02 -0500

    Assignment3

---
+++ Assignment_SK/assignment3_SK/simplejs/simple.js
@@ -0,0 +1,10 @@
+function change(value) {
+    document.getElementById("firstName").value = "Sri";
+
+    document.getElementById("lastName").value = "Kon";
+
+    document.getElementById("age").value = 25;
+
+    document.getElementById("gender").value = "Male";
+
+}
