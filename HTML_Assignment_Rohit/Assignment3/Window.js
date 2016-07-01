88d37cfabe84d1c399ed00c14ee0356959d0aa66
rohit-kavoori <rohitkavoori93@gmail.com>
2016-06-30 23:20:58 -0500

    Assignment3 added

---
+++ HTML_Assignment_Rohit/Assignment3/Window.js
@@ -0,0 +1,4 @@
+function showSize() {
+    var newWindow = window.open("", "New Window", "width=500,height=500");
+    newWindow.document.write("<p>Size of this new window is 500px for both height and width</p>");
+}
