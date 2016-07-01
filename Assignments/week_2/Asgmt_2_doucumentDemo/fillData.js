873482cb01ea3382c9ca97ffd087c706c866cdc1
sudha <sudha.reddy892@gmail.com>
2016-07-01 00:33:59 -0500

    Html,JS asgmt 2

---
+++ Assignments/week_2/Asgmt_2_doucumentDemo/fillData.js
@@ -0,0 +1,13 @@
+function fillData() {
+    document.getElementById("fname").value = "Sudha";
+    document.getElementById("lname").value = "Gunamgari";
+    document.getElementById("age").value = 23;
+    document.getElementById("gender").value = "Female";
+}
+
+function changeData() {
+    document.getElementById("fname").value = "Ayaan";
+    document.getElementById("lname").value = "Gunamgari";
+    document.getElementById("age").value = 1;
+    document.getElementById("gender").value = "Male";
+}
