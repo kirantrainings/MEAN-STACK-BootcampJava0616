b16bdf614d4bb56c87abf8c6558b94d4d44cadb6
jaganreddy0927 <jaganreddy0927@gmail.com>
2016-07-02 11:10:12 -0500

    All assignments together

---
+++ Assignments_Jagan/Java Script/Assignment4_Jagan/Task1/DynamicTableContent/js/jsondata.js
@@ -0,0 +1,13 @@
+function lookUpData() {
+    return {
+        employees:[ 
+{"name": "Jagan Reddy", "Designation": "Java Developer","Department": "Information Technaloy","Salary": "15000"},
+            
+{"name": "Vinay Reddy", "Designation": "UI Developer","Department": "Information Technaloy","Salary": "13000"},
+            
+ {"name": "Naveen", "Designation": " Jr. Java Developer","Department": "Information Technaloy","Salary": "10000"},
+
+{"name": "Y Naveen", "Designation": "Network Engineer","Department": "Information Technaloy","Salary": "9000"},
+ ]
+    };
+}
