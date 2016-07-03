eac5015b93d9e9af271e441ce0e475967632f9c2
speddirajubcj <speddiraju.bcj@gmail.com>
2016-07-02 22:57:37 -0500

    Dynamic Assignments

---
+++ Dynamic_JS/Task1/EmployeeData.js
@@ -0,0 +1,25 @@
+function lookUpData(){
+    return{
+        employees: [
+            { "Employee Name": "Shravya Peddiraju", 
+             "Department": "Developer", 
+             "Grade": "Manager"},
+            
+            { "Employee Name": "Monic Kalavandala", 
+             "Department": "Customer Services", 
+             "Grade": "Manager"},
+            
+            { "Employee Name": "Sudha Gunamgiri", 
+             "Department": "Accounts", 
+             "Grade": "Manager"},
+            
+            { "Employee Name": "Nikshitha Nagelly", 
+             "Department": "Marketing", 
+             "Grade": "Manager"},
+            
+            { "Employee Name": "Sravani Kyasaram", 
+             "Department": "Sales", 
+             "Grade": "Manager"}
+        ]
+    };
+}
