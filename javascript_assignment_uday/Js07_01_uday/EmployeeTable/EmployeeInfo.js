a1fb5fe26dfc243782dcd2f883c64351fc476dcc
uvandanapu <uvandanapu.bcj@gmail.com>
2016-07-04 22:23:01 -0500

    Add files via upload

---
+++ javascript_assignment_uday/Js07_01_uday/EmployeeTable/EmployeeInfo.js
@@ -0,0 +1,32 @@
+function lookUpData(){
+    return {
+        employees: [
+		
+            {
+                "Emp Name" : "Bose",
+                "Department" : "HR",
+                "Grade" : "A"
+            },
+            {
+                "Emp Name" : "Ranveer",
+                "Department" : "Sales",
+                "Grade" : "C"
+            },
+            {
+                "Emp Name" : "Ranbeer",
+                "Department" : "R&D",
+                "Grade" : "B"
+            },
+            {
+                "Emp Name" : "Pawan",
+                "Department" : "Finance",
+                "Grade" : "B"
+            },
+            {
+                "Emp Name" : "Akira",
+                "Department" : "Marketing",
+                "Grade" : "A"
+            }
+        ]
+    };
+}
