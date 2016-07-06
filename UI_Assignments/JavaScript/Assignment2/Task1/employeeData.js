9789669f06b50de9cecd8868ff242aee08f513b8
lohith55 <lthota.bcj@gmail.com>
2016-07-06 02:29:23 -0500

    javascript Assignments

---
+++ UI_Assignments/JavaScript/Assignment2/Task1/employeeData.js
@@ -0,0 +1,25 @@
+function lookUpData(){
+    return{
+        employees: [
+            { "Employee Name": "Jagan Reddy Baddam", 
+             "Department": "Marketing", 
+             "Grade": "Senior Manager"},
+            
+            { "Employee Name": "Srinivas Kondapaneni", 
+             "Department": "Accounting", 
+             "Grade": "Senior Manager"},
+            
+            { "Employee Name": "Lohith Thota", 
+             "Department": "Customer Services", 
+             "Grade": "Manager"},
+            
+            { "Employee Name": "Suneendra Banda", 
+             "Department": "Marketing", 
+             "Grade": "Manager"},
+            
+            { "Employee Name": "Naveen Kurakula", 
+             "Department": "Sales", 
+             "Grade": "Manager"}
+        ]
+    };
+}
---
+++ UI_Assignments/JavaScript/Assignment3/validations.js
@@ -0,0 +1,48 @@
+function validators(){
+    var numbersOnly, alphabetsOnly, alphaNumerics;
+    
+    return{
+        numbersOnly : function(evt){
+            if(evt.keyCode >= 48 && evt.keyCode <= 57){
+                
+            }
+            else{
+                evt.preventDefault();
+            }
+        },
+
+        alphabetsOnly : function(evt){
+            if((evt.keyCode >= 65 && evt.keyCode <= 90) || (evt.keyCode >= 97 && evt.keyCode <= 122)){
+                
+            }
+            else{
+                evt.preventDefault();
+            }
+            
+        },
+
+        alphaNumerics : function(evt){
+            if((evt.keyCode >= 65 && evt.keyCode <= 90) || (evt.keyCode >= 97 && evt.keyCode <= 122) ||
+               (evt.keyCode >= 48 && evt.keyCode <= 57) || evt.keyCode == 38 || 
+               (evt.keyCode >= 44 && evt.keyCode <= 47) || evt.keyCode == 32){
+                
+            }
+            else{
+                evt.preventDefault();
+            }
+               
+        },
+        
+        changeDropDown : function(){
+            getControls().states.innerHTML = "";
+            var selected = getControls().countries.value;
+            if(selected == "US" ){
+                buildStates();
+            }
+            else if(selected == "IN"){
+                buildStates();
+            }
+        },
+    };
+    
+}
