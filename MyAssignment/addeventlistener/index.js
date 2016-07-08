12d802f4902b41f23f5f1898a955c427a225745c
bhimdahal-bcj <bdahal.bcj@gmail.com>
2016-07-08 08:44:19 -0500

    adding event binding assignment

---
+++ MyAssignment/addeventlistener/index.js
@@ -0,0 +1,68 @@
+function controls() {
+    return {
+        countries: document.getElementById("ddlCountry"),
+        states: document.getElementById("ddlStates")
+    };
+}
+
+
+function buildCountries() {  
+    var countryList = lookUpData().countries;
+    for (var i = 0; i < countryList.length; i++) {
+        buildOptionTag(countryList[i]);
+    }
+}
+
+function buildStates() {  
+    var usList = lookUpUsStates().states;
+    return {
+        us: function(){
+            for (var i = 0; i < usList.length; i++) {
+            buildStatesOptionTag(usList[i]);
+                console.log(usList);
+            }
+        }
+    }
+    
+}
+
+function buildStatesOptionTag(states) {
+    var optionTag = document.createElement("option");
+    optionTag.value = states.abbreviation;
+    optionTag.textContent = states.name;
+    controls().states.appendChild(optionTag);
+    console.log(optionTag);
+}
+
+function bindStates(){
+    var control = controls().countries;
+    var states = buildStates();
+    control.addEventListener("click", function(){
+        var options = control.querySelectorAll("option");
+        var count = options.length;
+        if(typeof(count) == "undefined" || count<1 ){
+            buildCountries();
+        }
+        
+    });
+    control.addEventListener("change", function(){
+        if(control.value == "United States"){
+            states.us;
+        }
+        
+    });
+}
+
+function buildOptionTag(country) {
+    var optionTag = document.createElement("option");
+    optionTag.value = country.code;
+    optionTag.textContent = country.name;
+    controls().countries.appendChild(optionTag);
+}
+
+function initializePage() {
+    buildCountries();
+    bindStates();
+}
+
+initializePage();
