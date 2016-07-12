b7ff31aa44199f71965be68e1b7125760769b3fa
Shravani <kyasaram.shravani@gmail.com>
2016-07-12 14:57:00 -0500

    removed duplicated directory

--- Assignment_Shravani/Assignment_html/Assignment3/window.js
+++
@@ -1,4 +0,0 @@
-function showSize() {
-    var newWindow = window.open("", "New Window", "width=300,height=300");
-    newWindow.document.write("<p>Hello! This si new window. Size of this new window is 300px for both height and width</p>");
-}
