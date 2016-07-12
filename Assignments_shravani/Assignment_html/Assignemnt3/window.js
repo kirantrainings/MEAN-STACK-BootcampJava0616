c551d2ba831cbef205d00050bb30f8c9e5f7d10c
Shravani <kyasaram.shravani@gmail.com>
2016-07-12 15:30:43 -0500

    html assignment

---
+++ Assignments_shravani/Assignment_html/Assignemnt3/window.js
@@ -0,0 +1,4 @@
+function showSize() {
+    var newWindow = window.open("", "New Window", "width=300,height=300");
+    newWindow.document.write("<p>Hello! This si new window. Size of this new window is 300px for both height and width</p>");
+}
---
+++ Assignments_shravani/Assignment_html/Assignment2/change.js
@@ -0,0 +1,10 @@
+function change(){
+    var fname = "shravani";
+    var lname = "kyasaram";
+    var age = "24";
+    btnradio = document.getElementById("female");
+    document.getElementById("fname").value = fname;
+    document.getElementById("lname").value = lname;
+    document.getElementById("age").value = age;
+    btnradio.checked = true;
+}
