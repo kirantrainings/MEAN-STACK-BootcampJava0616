4c6c8008c16a0b92673b475e422a4b93be6e80c0
jaganreddy0927 <jaganreddy0927@gmail.com>
2016-07-27 08:16:57 -0500

    eshop project

---
+++ Assignments_Jagan/UI Project/eShop/app/models/cart.js
@@ -0,0 +1,12 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var cart = new Schema({
+    productName: String,
+    quantity: Number
+    
+});
+
+
+
+module.exports= mongoose.model("cart",cart);
